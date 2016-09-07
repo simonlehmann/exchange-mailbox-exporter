6e08558bd869c31473af29b1018ae1170fb88113
Simon Lehmann <slehmann36@gmail.com>
2016-09-07 10:33:58 +0800

    Update MBExport.ps1
    Added copyright notice

--- MBExport.ps1
+++ MBExport.ps1
@@ -1,4 +1,23 @@
-﻿### Exchange 2013 Mailbox Exporter by Simon Lehmann ###
+#  Mailbox Export
+#  Version 1.0.2
+#
+# Run bulk mailbox exports as a scheduled task.
+# Copyright (C) 2015 Simon Lehmann
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 # Script version
 $ver = 1.0.2
 
